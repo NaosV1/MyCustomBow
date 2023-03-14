825e34ac3f855cda91ccc8d3147ef6bd000484e7
BuildTools <unconfigured@null.spigotmc.org>
2023-03-14 18:31:24 +0100

    Initial commit

---
+++ src/main/java/fr/naos/mycustombow/MyCustomBow.java
@@ -0,0 +1,17 @@
+package fr.naos.mycustombow;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MyCustomBow extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/fr/naos/mycustombow/MyCustomBowListener.java
@@ -0,0 +1,2 @@
+package fr.naos.mycustombow;public class MyCustomBowListener {
+}
