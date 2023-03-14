c91393572304053323ad01709f2f211fa073324e
BuildTools <unconfigured@null.spigotmc.org>
2023-03-14 18:44:49 +0100

    adding a update checker

--- src/main/java/fr/naos/mycustombow/MyCustomBow.java
+++ src/main/java/fr/naos/mycustombow/MyCustomBow.java
@@ -1,7 +1,13 @@
 package fr.naos.mycustombow;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public final class MyCustomBow extends JavaPlugin {
 
@@ -17,6 +23,32 @@ public final class MyCustomBow extends JavaPlugin {
         pm.registerEvents(new MyCustomBowListener(), this);
         pm.registerEvents(new bowShootListener(), this);
         cData.load();
+        updateCheck();
+    }
+
+    private void updateCheck(){
+        try {
+            URL url = new URL("https://api.spigotmc.org/legacy/update.php?resource=108616");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setConnectTimeout(5000);
+            con.setReadTimeout(5000);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            if (!(content.toString().equals(getDescription().getVersion()))) {
+                Bukkit.getConsoleSender().sendMessage("\n§a[MyCustomBow] §fA new version is available\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=\n§fYour version : §b" + getDescription().getVersion() + "\n§fNew version : §b" + content + "\n§fDownload it here :§e https://www.spigotmc.org/resources/mycustombow.108616/\n§fIf you §clike §fthe plugin, please leave a review, it help a lot !\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=\n");
+            }
+        } catch (Exception e) {
+            Bukkit.getConsoleSender().sendMessage("§cError checking update for ArmorStand Manager: §e" + e.getMessage());
+        }
+
     }
 
     public static MyCustomBow getInstance() {
